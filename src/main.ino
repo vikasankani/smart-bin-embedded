5c719cce7bf73295e047fd3f5bdc7d181e87d936
vikasankani <106400236+vikasankani@users.noreply.github.com>
2026-04-24 12:28:46 +0530

    Update main.ino

--- src/main.ino
+++ src/main.ino
@@ -1,1 +1,40 @@
+#include <Servo.h>
 
+#define TRIG 5
+#define ECHO 18
+#define SERVO_PIN 19
+
+Servo myServo;
+
+void setup() {
+  pinMode(TRIG, OUTPUT);
+  pinMode(ECHO, INPUT);
+  myServo.attach(SERVO_PIN);
+  myServo.write(0);
+
+  Serial.begin(115200);
+}
+
+float getDistance() {
+  digitalWrite(TRIG, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG, LOW);
+
+  long duration = pulseIn(ECHO, HIGH);
+  float distance = duration * 0.034 / 2;
+  return distance;
+}
+
+void loop() {
+  float d = getDistance();
+
+  if (d < 20) {
+    myServo.write(90);
+  } else {
+    myServo.write(0);
+  }
+
+  delay(200);
+}
